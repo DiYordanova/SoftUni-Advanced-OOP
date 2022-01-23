13b5db32e2acdaf99f77af599c0613c98ecbe84a
Dilyana <d-yordanova@mail.bg>
2022-01-23 15:17:42 +0200

    Added solutions.

---
+++ Exceptions/EnterNumbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace EnterNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int end = 100;
+
+            int[] arr = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    arr[i] = int.Parse(Console.ReadLine());
+
+                    if (arr[i] <= start || arr[i] > end)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                }
+
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Number!");
+                    i--;
+                    continue;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine($"Your number is not in range {start} - 100!");
+                    i--;
+                    continue;
+                }
+
+                start = arr[i];
+            }
+            Console.Write(string.Join(", ", arr));
+        }
+    }
+}
---
+++ Exceptions/SquareRoot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            try
+            {
+                n = int.Parse(Console.ReadLine());
+                if (n < 0)
+                {
+                    throw new Exception();
+                }
+                Console.WriteLine(Math.Sqrt(n));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
