9ea73bf755d11723e4a6d7eb03f6d52117a4d026
Dilyana <d-yordanova@mail.bg>
2022-01-23 13:31:39 +0200

    Added solutions.

---
+++ Inheritance/Restaurant/Coffee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    class Coffee : HotBeverage
+    {
+        private const decimal DefaultPrice = 3.5M;
+
+        private const double DefaultMilliliters = 50;
+
+        public Coffee(string name, double coffeine)
+            : base(name, DefaultPrice, DefaultMilliliters)
+        {
+            this.Coffeine = coffeine;
+        }
+
+        public double Coffeine { get; set; }
+    }
+}
